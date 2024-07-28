dcc0e8a1c2637162ea7fc9d2be8646879d97d38e
jackie4u <jackie4u@seznam.cz>
2024-07-28 16:38:01 +0200

    Init MAUI App

---
+++ Lister/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Lister
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Lister/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Lister
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
